80a7166de408d17a3768c3b0214224686e0014de
Amber <amber.waltens@hotmail.com>
2020-04-11 10:37:19 +0200

    mongoose and schema added, working with hard coded data

--- controllers/api/v1/stats.js
+++ controllers/api/v1/stats.js
@@ -1,22 +1,44 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const statSchema = new Schema({ 
+   country: String,
+   numberCases: Number 
+})
+const Stat = mongoose.model('Stat', statSchema)
+
 const getAll = (req, res) => {
-    res.json({
-        "status" : "success",
-        "data" : {
-            "stats" : []
+    Stat.find((err, doc) =>{
+        if(!err){
+            res.json({
+                "status" : "success",
+                "data" : {
+                    "stats" : doc
+                }
+            })
         }
     })
+
+    
 }
 
 const create = (req, res) => {
-    res.json({
-        "status" : "success",
-        "data" : {
-            "stats": {
-                "country" : "belgium",
-                "number" : 200
-            }
+    let stat = new Stat()
+    stat.country = "belgium"
+    stat.numberCases = 200
+    stat.save((err, doc)=>{
+        if (!err) {
+            res.json({
+                "status" : "success",
+                "data" : {
+                    "stats": {
+                        doc
+                    }
+                }
+            })
         }
     })
+
+    
 }
 
 module.exports.getAll = getAll
