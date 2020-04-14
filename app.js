c1b3c6563e951b9bd434a93b6b4fa22c0386ee4a
Amber <amber.waltens@hotmail.com>
2020-04-14 12:44:07 +0200

    cors installed and implemented

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors')
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const apiStatsRouter = require('./routes/api/v1/stats')
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1/stats', apiStatsRouter);
