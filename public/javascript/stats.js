de68612a8973de207c0046d75b2b7393cc1ecb89
Amber <amber.waltens@hotmail.com>
2020-04-14 15:14:40 +0200

    style added

--- public/javascript/stats.js
+++ public/javascript/stats.js
@@ -28,7 +28,7 @@ function appendInfo() {
             let country = stat.country
             let numberCases = stat.numberCases
             let p = document.createElement('p')
-            let text = document.createTextNode(`${country} --> ${numberCases}`)
+            let text = document.createTextNode(`${country}:   ${numberCases}`)
             p.appendChild(text)
             let overview = document.querySelector("#overview")
             overview.appendChild(p)
