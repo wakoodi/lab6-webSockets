525144d7319f01e5ca24c82b1b48e19255ffc87c
Amber <amber.waltens@hotmail.com>
2020-04-14 12:23:34 +0200

    js logic added to stats.js

--- public/javascript/stats.js
+++ public/javascript/stats.js
@@ -1,4 +1,5 @@
 const base_url = "https://lab6-stats.herokuapp.com/"
+let overview = document.querySelector("#overview");
 
 primus = Primus.connect(base_url, {
     reconnect: {
@@ -8,3 +9,30 @@ primus = Primus.connect(base_url, {
     }
 })
 
+primus.on("data", (json) => {
+    if(json.action === "addStats"){
+        overview.innerHTML = ""
+        appendInfo()
+    }
+})
+
+function appendInfo() {
+    fetch(base_url + "/api/v1/stats", {
+        method: "get",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }).then(response => {
+        return response.json();
+    }).then(json => {
+        json.data.forEach(stat => {
+            let div = document.createElement('div').setAttribute("class", "statistic")
+            let country = document.createElement('h2').innerHTML = stat.country
+            let numberCases = document.createElement('p').innerHTML = stat.numberCases
+            div.append(country).append(numberCases)
+            overview.append(div);
+        });
+    })
+}
+
+window.onload = appendInfo();
