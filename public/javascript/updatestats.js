5f60c526630aa39962f6bf5559b2b9605bfd2f11
Amber <amber.waltens@hotmail.com>
2020-04-27 15:32:40 +0200

    added .then with primus write at updatestats.js

--- public/javascript/updatestats.js
+++ public/javascript/updatestats.js
@@ -1,7 +1,7 @@
 const base_url = "https://lab6-stats.herokuapp.com"
 
 
-primus = Primus.connect(base_url, {
+let primus = Primus.connect(base_url, {
     reconnect: {
         max: Infinity,
         min: 500,
@@ -13,21 +13,23 @@ document.querySelector("#submit").addEventListener("click", function (e) {
     let country = document.querySelector("#country").value
     let numberCases = document.querySelector("#number").value
 
-    fetch(base_url + "/api/v1/stats/updatestats", {
+    fetch(base_url + "/api/v1/stats/"+ country, {
         method: "put",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            "country": country,
             "numberCases": numberCases
         })
+    }).then((e)=>{
+        if(e.status === 200){
+            primus.write({ country: country, numberCases: numberCases })
+        }
     }).catch(err => {
         console.log(err);
     })
 
-    primus.write({ "action": "update" })
-
+    //primus.write({ "action": "update" })
 
     e.preventDefault()
 })
